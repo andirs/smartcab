b534dcb513370ea26d726a6ee6ca8207058770d2
andirs <andirs@gmail.com>
2016-10-19 12:43:11 -0700

    Further commentation and statistics

--- smartcab/rl_agent.py
+++ smartcab/rl_agent.py
@@ -53,7 +53,7 @@ class LearningAgent(Agent):
                 Initialize state with a default-value.
             If state has been seen before:
                 Decide with a certain randomness whether to
-                take the best action available or a comlete random action
+                take the best action available or a comlete random action.
         '''
         q_start = 0
         action_set = [None, 'forward', 'left', 'right']
@@ -62,7 +62,9 @@ class LearningAgent(Agent):
         random_threshold = random.random()
         # Sigmoid function for epsilon that moves towards 
         # 1 with high time step t
-        self.epsilon = (1 / (1 + math.exp(-t)))
+        #self.epsilon = (1 / (1 + math.exp(-t)))
+        # 'Slower' sigmoid function
+        self.epsilon = (1 / (1 + math.exp((-t/2))))
         # Check if state exists in qtable already
         if qtable.has_key(state):
             # Check if random threshold is smaller or equal epsilon
@@ -74,6 +76,7 @@ class LearningAgent(Agent):
                 action = random.choice(action_set.keys())
             else:
             	# Occasionally perform random action
+                # to explore world especially in the beginning
                 action = random.choice(action_set)
         else:
         	# Initialize new state using q_start
@@ -83,8 +86,17 @@ class LearningAgent(Agent):
 
     def learn_policy(self, qtable, state, alpha, gamma, t):
         '''
-        Learn policy with set alpha and gamma.
+        Learn policy based on alpha, gamma 
+        and previous rewards.
         '''
+        # Sigmoid function that moves towards zero for high t
+        # alpha = (1 / (1+math.exp(t)))
+        # 'Slower' sigmoid function
+        alpha = (1 / (1+math.exp((t/2))))
+
+        # Slow sigmoid function for gamma
+        gamma = (1 / (1+math.exp((t/4)))+3)
+
         if self.trial_count > 0:
             #alpha = alpha/float(self.trial_count)
             q_new = qtable[self.prev_state][self.prev_action]
@@ -92,6 +104,11 @@ class LearningAgent(Agent):
             qtable[self.prev_state][self.prev_action] = q_new
 
     def update(self, t):
+        '''
+        Combine determination of action, calculation of reward
+        and learning process of agent.
+        Afterwards, check if agent has reached goal and report.
+        '''
         # Gather inputs for current state
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
@@ -109,7 +126,6 @@ class LearningAgent(Agent):
 
         # Find best action to take given current state
         action = self.best_action(self.qtable, self.state, t)
-        probabilities = [self.qtable[(self.state, None)], self.qtable[(self.state, 'forward')], self.qtable[(self.state, 'left')], self.qtable[(self.state, 'right')]]
 
         # Execute action and get reward
         reward = self.env.act(self, action)
@@ -117,7 +133,8 @@ class LearningAgent(Agent):
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
         # Learn policy based on state, alpha, gamma and t
-        self.learn_policy(self.qtable, self.state, self.alpha, self.gamma, t)
+        self.learn_policy(
+            self.qtable, self.state, self.alpha, self.gamma, t)
 
         # Remember state, action & reward for learning
         self.prev_state = self.state
@@ -140,6 +157,7 @@ class LearningAgent(Agent):
         self.trial_count += 1
 
     def feature_comparison(self, 
+        output,
         alpha = [.05, .3, .6, .9, 1], 
         gamma = [.05, .3, .6, .9, 1],
         epsilon = [.05, .3, .6, .9, 1],
@@ -152,27 +170,21 @@ class LearningAgent(Agent):
         Saves output in static file in smartcab/data/
         '''
         import os
-        o_dir = 'smartcab/data'
 
+        # Set folder for output
+        o_dir = 'smartcab/data'
         if not os.path.exists(o_dir):
             os.makedirs(o_dir)
 
+        # For naming conventions
         alpha_list = alpha
         gamma_list = gamma
         epsilon_list = epsilon
 
+        # Create container for the whole summary
         feature_summary = []
-        '''
-        feature_summary = pd.DataFrame(
-            columns = [
-            'alpha', 
-            'gamma', 
-            'epsilon', 
-            'no_success', 
-            'success', 
-            'steps'])
-        '''
 
+        # Create combinations for each of the possible settings
         for alpha in alpha_list:
             for gamma in gamma_list:
                 for epsilon in epsilon_list:
@@ -181,7 +193,7 @@ class LearningAgent(Agent):
                     	'no_success', 
                     	'success', 
                     	'steps'])
-                    combination_name = [alpha, gamma, epsilon] # if needed
+
                     e = Environment()
                     a = e.create_agent(LearningAgent)
 
@@ -218,7 +230,7 @@ class LearningAgent(Agent):
                     df = [alpha, gamma, epsilon, temp_nsuc_mean, temp_suc_mean,temp_step_mean]
                     feature_summary.append(df)
         
-
+        # Turn summary into data frame
         summary_comp = pd.DataFrame(feature_summary, 
         	columns = [
         		'alpha',
@@ -227,7 +239,8 @@ class LearningAgent(Agent):
         		'no_success',
         		'success',
         		'steps'])
-        summary_comp.to_csv(o_dir + '/rl_agent_feature_comparison.csv', header = True, index = None, sep = ';', mode = 'a')
+        # Write data frame to disk
+        summary_comp.to_csv(o_dir + '/' + output, header = True, index = None, sep = ';', mode = 'a')
 
 
 
@@ -245,7 +258,10 @@ def run():
     sim = Simulator(e, update_delay=0, display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
+    ###
+    ### Run the program
 
+    # Stats for trial summary
     success_summary = pd.DataFrame(index = ['no_success', 'success', 'steps'])
     validation_no = 5
 
@@ -254,30 +270,51 @@ def run():
         sim.run(n_trials=100)  # run for a specified number of trials
         # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
+        # Print trial count and create stats for single trial
         print "Trial Count: ", a.trial_count
         success_temp = pd.DataFrame.from_dict(a.trial_summary, orient='index')
         success_temp.index = ['no_success', 'success', 'steps']
         temp_column_name = 'trial_count_' + str(i+1)
         success_summary[temp_column_name] = success_temp
+        # Reset statistic data for each trial
         a.trial_summary[0] = 0
         a.trial_summary[1] = 0
         a.trial_summary[2] = 0
 
-    print a.probabilities
-
-    for row in a.qtable.iteritems():
-        print row
     print success_summary
     success_average = success_summary.mean(axis=1)[0:]
     print "Average: "
     print success_average
     print "Accuracy: ", min(success_average) / max(success_average) 
-    print 
-    print
 
+    ###
+    ### Perform feature comparison with a variety of settings
+    ### to find best choice of alpha, gamma and epsilon.
+
+    #### Stats with sigmoid function for epsilon ####
+    # To activate the sigmoid function, uncomment line 65
+
+    #a.feature_comparison(output='feature_comparison_e_greedy_sig_e.csv', epsilon=[1])
+
+    #### Stats with sigmoid function for epsilon ####
+    #### and neg sigmoid function for alpha ####
+    # To activate the sigmoid function for epsilon, uncomment line 65
+    # To activate the sigmoid function for alpha, uncomment line 93
+    #a.feature_comparison(output='feature_comparison_ae_sig.csv', epsilon=[1], alpha=[1])
+
+    #### Stats with slower sigmoid function for epsilon ####
+    #### and slower neg sigmoid function for alpha ####
+    # To activate the sigmoid function for epsilon, uncomment line 67
+    # To activate the sigmoid function for alpha, uncomment line 95
+    #a.feature_comparison(output='feature_comparison_ae_slow_sig.csv', epsilon=[1], alpha=[1])
+
+    a.feature_comparison(output='feature_comparison_aeg_slow_sig.csv', epsilon=[1], alpha=[1], gamma=[1])
+
+    #### Stats without sigmoid function for epsilon ####
+    # To deactivate sigmoid function turn line 65 into a comment
     
-    #a.feature_comparison(alpha=[0.9, 1], gamma=[0.9, 1], epsilon=[0.9, 1], cv=2,ntrials=100)
-    #a.feature_comparison(epsilon=[1])
+    #a.feature_comparison(output='feature_comparison_e_greedy.csv')
+    #a.feature_comparison(output='feature_comparison_all.csv')
 
 if __name__ == '__main__':
     run()
